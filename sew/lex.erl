b55228690f3ab6ce155777ecc1ca77d1196bf0a0
EtoOnAmill <alezardo12@gmail.com>
2024-08-04 15:20:16 +0100

    removed tokens not relevant to grammar in lex return

--- sew/lex.erl
+++ sew/lex.erl
@@ -3,9 +3,11 @@
 
 %% string -> list tokens
 lex([]) -> [];
-lex(Str) -> 
-    {Ttype, Token, Rest} = lex_one(Str),
-    [{Ttype,Token}|lex(Rest)].
+lex(Str) ->
+    case  lex_one(Str) of
+        {ignore, _, Rest} -> lex(Rest);
+        {Ttype, Token, Rest} -> [{Ttype,Token}|lex(Rest)]
+    end.
 
 %% string * string -> {token , string}
 lex_one([]) -> {"", []};
@@ -13,46 +15,50 @@ lex_one([Chr|Tail]) ->
     WhiteSpace=[$\s,$\t,$\n,$\v,$\r],
     Reserved=[$^,$!,$%,$/,$|,$&,$:,$[,$],$=,$>,$;,$~,$`,$.,$,,$#|WhiteSpace],
 
-    case Chr of
-        $` -> 
-            IsntBackTick = fun($`) -> false; (_) -> true end,
-            {Token, Rem} = case take_while(IsntBackTick, Tail) of 
-                {T, [$`|Rest]} -> {T, Rest};
-                _ -> throw("Unclosed delimiter, expected '`'") 
-            end,
-            {string, Token, Rem};
-        $# -> 
-            IsntOctothorp = fun($#) -> false; (_) -> true end,
-            {Token, Rem} = case take_while(IsntOctothorp, Tail) of 
-                {T, [$#|Rest]} -> {T, Rest};
-                {T, []} -> {T, []};
-                _ -> throw("Unclosed delimiter, expected '#'") 
-            end,
-            {comment, Token, Rem};
-        C  -> case lists:member(C, Reserved) of 
-            true -> case [Chr|Tail] of
-                [$:,$:|TTail] -> {reserved, "::", TTail};
-                [$>,$>|TTail] -> {reserved, ">>", TTail};
-                [$=,$:|TTail] -> {reserved, "=:", TTail};
-                [$:,$=|TTail] -> {reserved, ":=", TTail};
-                [$^,$]|TTail] -> {reserved, "^]", TTail};
-                [$%,$]|TTail] -> {reserved, "%]", TTail};
-                [$/,$]|TTail] -> {reserved, "/]", TTail};
-                [$!,$]|TTail] -> {reserved, "!]", TTail};
-                _ -> {reserved, C, Tail}
-            end;
-            false ->
-                IsntReserved = fun(CC) -> not lists:member(CC, Reserved) end,
-                erlang:insert_element(1, take_while(IsntReserved,  [Chr|Tail]), word)  
-            end
-    end.
+    case lists:member(Chr, WhiteSpace) of
+        true -> {ignore, whiteSpace, Tail};
+        false ->
+        (case Chr of
+            $` ->
+                IsntBackTick = fun($`) -> false; (_) -> true end,
+                {Token, Rem} = case take_while(IsntBackTick, Tail) of
+                    {T, [$`|Rest]} -> {T, Rest};
+                    _ -> throw("Unclosed delimiter, expected '`'")
+                end,
+                {string, Token, Rem};
+            $# ->
+                IsntOctothorp = fun($#) -> false; (_) -> true end,
+                {Token, Rem} = case take_while(IsntOctothorp, Tail) of
+                    {T, [$#|Rest]} -> {T, Rest};
+                    {T, []} -> {T, []};
+                    _ -> throw("Unclosed delimiter, expected '#'")
+                end,
+                {ignore, Token, Rem};
+            C  -> case lists:member(C, Reserved) of
+                true -> case [Chr|Tail] of
+                    [$:,$:|TTail] -> {reserved, "::", TTail};
+                    [$>,$>|TTail] -> {reserved, ">>", TTail};
+                    [$=,$:|TTail] -> {reserved, "=:", TTail};
+                    [$:,$=|TTail] -> {reserved, ":=", TTail};
+                    [$^,$]|TTail] -> {reserved, "^]", TTail};
+                    [$%,$]|TTail] -> {reserved, "%]", TTail};
+                    [$/,$]|TTail] -> {reserved, "/]", TTail};
+                    [$!,$]|TTail] -> {reserved, "!]", TTail};
+                    _ -> {reserved, [C], Tail}
+                    end;
+                false ->
+                    IsntReserved = fun(CC) -> not lists:member(CC, Reserved) end,
+                    erlang:insert_element(1, take_while(IsntReserved,  [Chr|Tail]), word) 
+                end
+            end)
+        end.
 
 %% (a -> bool) -> list a -> {list a, list a}
-take_while(Fn, List) -> 
+take_while(Fn, List) ->
     Helper = fun
         Hlp([], Acc) -> {lists:reverse(Acc),[]};
-        Hlp([Chr|Rest]=Whole, Acc) -> case Fn(Chr) of 
-            true -> Hlp(Rest, [Chr|Acc]); 
+        Hlp([Chr|Rest]=Whole, Acc) -> case Fn(Chr) of
+            true -> Hlp(Rest, [Chr|Acc]);
             false -> {lists:reverse(Acc), Whole} end
     end,
     Helper(List, []).
--- sew/sew.erl
+++ sew/sew.erl
@@ -1,8 +1,12 @@
 -module(sew).
 -export([comp/1]).
 
-comp(Filename) -> 
+%%]#fhaslskjdh#
+comp(Filename) ->
     case file:read_file(Filename) of
-        {ok, Content} -> io:format("Compiled ~s succsessfuly~n", [Filename]);
+        {ok, Content} ->
+            Lexed = lex:lex(bitstring_to_list(Content)),
+            io:format("Lexed succsessfuly:~p~n", [Lexed]),
+            io:format("Compiled ~s succsessfuly~n", [Filename]);
         Err -> io:fwrite("Failed with error ~p~n", [Err])
     end.
